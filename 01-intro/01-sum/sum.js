d68be69e7d38b41e09ca7572b92a62d96f8f1cac
Mikhail Yunikov <mikhail.yunikov@gmail.com>
2022-03-03 17:15:49 +0400

    first task solution

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,14 @@
 function sum(a, b) {
   /* ваш код */
+  // return (a + b);
+  
+  if ((typeof(a) === 'number') && (typeof(b) === 'number')){
+      return (a + b);
+  }
+  else {
+    //throw "TypeError";
+    throw new TypeError();
+  }
 }
 
 module.exports = sum;
